e5033b63b8c9351950a65947c102519a86e0c26c
Matteo Bacci <matteo.bacci@antreem.com>
2020-04-18 11:26:01 +0200

    Added analytics

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from "react";
 import Loader from "./components/Loader";
 import Main from "./Main";
 import fetchData from "./fetchData";
+import ReactGA from "react-ga";
 
 const App = () => {
+  ReactGA.initialize(process.env.REACT_APP_GA);
+  ReactGA.set({ anonymizeIp: true });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   const data = fetchData();
   if (!data) return <Loader />;
 
